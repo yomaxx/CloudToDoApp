bb58fcd2410cf737c0b9140e0d2664431cce0d08
yomaxx <max.valkenburg@hotmail.com>
2023-11-07 09:18:11 +0100

    updated carrousel

--- backend/routes/carrousel.routes.js
+++ backend/routes/carrousel.routes.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const carrouselRouter = express.Router();
-const carrouselData = require('./../data/carrousel.json');
+//const carrouselData = require('./../data/carrousel.json');
+
+const { DOMParser } = require('xmldom');
+const fetch = require('node-fetch');
+const bucketName = process.env.BUCKET_NAME;
+const endpointUrl = `https://${bucketName}.s3.amazonaws.com`;
 
 carrouselRouter.get('', (req, res) => {
-    res.json(carrouselData);
+const objectUrls = [];
+  fetch(endpointUrl)
+  .then(response => response.text())
+  .then(xmlString => {
+    const parser = new DOMParser();
+    const xmlDocument = parser.parseFromString(xmlString, 'application/xml');
+    const contentlist = xmlDocument.getElementsByTagName('Key');
+    
+    // Loop over key values and create urls
+    for (let i = 0; i < contentlist.length; i++) {
+        const objectUrl = `${endpointUrl}/${contentlist[i].textContent}`;
+        objectUrls.push({url: objectUrl});
+    }
+    res.json(objectUrls);
+  })
+  .catch(err => console.error(err));
 });
-
-
-module.exports = carrouselRouter;
