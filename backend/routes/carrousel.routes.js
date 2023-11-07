80d1a0f2f3d7357ad0e63bba7c957ef88ebd0131
yomaxx <max.valkenburg@hotmail.com>
2023-11-07 22:44:01 +0100

    updated carrousel

--- backend/routes/carrousel.routes.js
+++ backend/routes/carrousel.routes.js
@@ -7,7 +7,7 @@ AWS.config.update({
   accessKeyId: '${AWS_ACCESS_KEY_ID}}',
   secretAccessKey: '${AWS_SECRET_ACCESS_KEY}',
   SessionToken: '${AWS_SESSION_TOKEN}',
-  region: 'us-east-1'
+  //region: 'us-east-1'
 });
 
 const s3 = new AWS.S3();
